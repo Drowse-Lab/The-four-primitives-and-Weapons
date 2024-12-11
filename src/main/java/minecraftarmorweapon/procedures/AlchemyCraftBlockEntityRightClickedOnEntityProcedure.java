8e47fb87bc99fcbd1d92490b6d248cc43925dc3b
0526ngs <78349683+hrmcngs@users.noreply.github.com>
2024-12-11 22:15:43 +0900

    version up

--- src/main/java/minecraftarmorweapon/procedures/AlchemyCraftBlockEntityRightClickedOnEntityProcedure.java
+++ src/main/java/minecraftarmorweapon/procedures/AlchemyCraftBlockEntityRightClickedOnEntityProcedure.java
@@ -1,18 +1,12 @@
 package minecraftarmorweapon.procedures;
 
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 
-import minecraftarmorweapon.init.MinecraftArmorWeaponModItems;
-
 public class AlchemyCraftBlockEntityRightClickedOnEntityProcedure {
-	public static void execute(Entity entity, Entity sourceentity) {
-		if (entity == null || sourceentity == null)
+	public static void execute(Entity sourceentity) {
+		if (sourceentity == null)
 			return;
-		if ((sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == MinecraftArmorWeaponModItems.DESPORN_KENTI.get()) {
-			if (!entity.level.isClientSide())
-				entity.discard();
-		}
+		if (!sourceentity.level.isClientSide())
+			sourceentity.discard();
 	}
 }
