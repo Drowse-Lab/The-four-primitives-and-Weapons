ba80bc735a3ffb0717d51c8b6ba1c4be956010e4
0526ngs <78349683+hrmcngs@users.noreply.github.com>
2025-03-11 12:23:46 +0900

    Update OldKatanaItem.java

--- src/main/java/minecraftarmorweapon/item/OldKatanaItem.java
+++ src/main/java/minecraftarmorweapon/item/OldKatanaItem.java
@@ -57,7 +57,7 @@ public class OldKatanaItem extends SwordItem {
         return Component.literal("Old Sword")
             .setStyle(Style.EMPTY
             .withFont(new ResourceLocation("minecraft", "illageralt")) // フォント指定
-            .withColor(TextColor.parseColor("#5477b3")) // 緑色 (#55FF55)
+            .withColor(TextColor.parseColor("#5477b3")) // 淡い青色 (#5477b3)
             .withItalic(false)); // イタリックを無効化
     }
 	
