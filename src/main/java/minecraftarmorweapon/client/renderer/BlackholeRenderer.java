d982d0a37bb5ff1cc2aefaf4f57313db3f6fc595
Hiromichi Nagase <hrmc.ngs@gmail.com>
2026-04-13 08:30:14 +0900

    ちょっとだけ更新

--- src/main/java/minecraftarmorweapon/client/renderer/BlackholeRenderer.java
+++ src/main/java/minecraftarmorweapon/client/renderer/BlackholeRenderer.java
@@ -14,6 +14,14 @@ import minecraftarmorweapon.entity.BlackholeEntity;
 import javax.annotation.Nullable;
 
 public class BlackholeRenderer extends GeoEntityRenderer<BlackholeEntity> {
+
+	// @RotationParams(Blackhole, model=minecraft_armor_weapon:geo/blackhole.geo.json)
+	public static float YAW_OFFSET = 0f; // Y軸回転
+	public static float PITCH_OFFSET = 0f; // X軸回転
+	public static float ROLL_OFFSET = 0f; // Z軸回転
+	public static float SCALE = 1.0f; // 表示サイズ
+	// @EndRotationParams
+
 	public BlackholeRenderer(EntityRendererProvider.Context renderManager) {
 		super(renderManager, new BlackholeModel());
 		this.shadowRadius = 0f;
